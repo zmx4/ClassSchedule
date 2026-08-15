988eb4cdb35d71a5bd962d87c6bb11e6de3b0349
Tick Zhong <1601858426@qq.com>
2026-08-15 16:20:34 +0800

    创建项目

---
+++ ClassSchedule/ViewModels/MainViewModel.cs
@@ -0,0 +1,8 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace ClassSchedule.ViewModels;
+
+public partial class MainViewModel : ViewModelBase
+{
+    [ObservableProperty] private string _greeting = "Welcome to Avalonia!";
+}
---
+++ ClassSchedule/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace ClassSchedule.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
